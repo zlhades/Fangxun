5729b62f518e74650afbdfd76356e6a929f25877
Benson.Zhang <zlhades@outlook.com>
2015-10-23 22:04:36 +0800

    modfiy by req

---
+++ js/common.js
@@ -0,0 +1,8 @@
+//$(document).bind("mobileinit", function() {
+//    //disable ajax nav
+//    $.mobile.ajaxLinksEnabled = false;
+//    $.mobile.ajaxFormsEnabled = false;
+//    $.mobile.ajaxEnabled = false;
+//});
+$.mobile.pageLoadErrorMessage = '网络不给力，请刷新重试';
+$.mobile.transitionFallbacks.slideout ="none"
